87cf71b5748e0c1ff4a0e2ff57de3007355e72d9
felipee-nascimento <felipe.cruz.stone@gmail.com>
2024-12-04 01:46:14 -0300

    SecurityConfig + CORS

--- src/main/java/br/com/gocook/configuration/CorsConfiguration.java
+++ src/main/java/br/com/gocook/configuration/CorsConfiguration.java
@@ -1,26 +1,53 @@
-package br.com.gocook.configuration;
+//package br.com.gocook.configuration;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+//
+////
+////    @Override
+////    public void addCorsMappings(CorsRegistry registry) {
+////        registry.addMapping("/**") // Permite todas as rotas
+////                .allowedOrigins("*")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Inclua OPTIONS, que é usado nas preflight requests
+////                .allowedHeaders("*") // Permite todos os cabeçalhos
+////                .allowCredentials(true); // Permite cookies, se necessário
+////    }
+//
+//    @Bean(name = "corsFilterChain")
+//    public SecurityFilterChain corsFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors().and() // Ativa CORS
+//                .csrf().disable(); // Desativa CSRF para testes
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*")); // Origens permitidas
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+//        configuration.setAllowedHeaders(Arrays.asList("*")); // Cabeçalhos permitidos
+//        configuration.setAllowCredentials(true); // Permite cookies/autenticação via credenciais
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration); // Aplica a configuração para todas as rotas
+//        return (CorsConfigurationSource) source;
+//    }
+//}
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebSecurity
-public class CorsConfiguration implements WebMvcConfigurer{
-
-
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**") // Permite todas as rotas
-                            .allowedOrigins("*") // Permite o front-end
-                            .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
-                            .allowedHeaders("*") // Permite todos os cabeçalhos
-                            .allowCredentials(true); // Permite cookies, se necessário
-                }
-}
--- src/main/java/br/com/gocook/infra/security/SecurityConfig.java
+++ src/main/java/br/com/gocook/infra/security/SecurityConfig.java
@@ -4,22 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
 
     @Autowired
     SecurityFilter securityFilter;
@@ -27,24 +25,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable()) // Desativa CSRF
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configura CORS
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sem estado
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                        .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/register").permitAll() // Permite esses endpoints
+                        .anyRequest().authenticated() // Todas as outras requisições precisam de autenticação
+                ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // Configuração de encoder de senha
     }
-
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500/", "http://localhost:8081")); // Origens permitidas
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Permite todos os cabeçalhos
+        configuration.setAllowCredentials(true); // Permite credenciais
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplica CORS a todas as rotas
+        return source;
     }
 }
